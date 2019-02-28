9f3d044788620960171eb91df4ba6627d4e55364
Daniel <daniel.velin.2001@hotmail.com>
2019-02-28 10:28:17 -0500

    Complete exercise 2

---
+++ exercise_02.py
@@ -0,0 +1,20 @@
+def reverse(number):
+    n_list = list(str(number))
+    number = ""
+    for i in range(1, len(n_list) + 1):
+        number += n_list[-i]
+
+    return number
+
+def isPalindrome(number):
+    n_to_check = int(reverse(number))
+
+    if n_to_check == int(number):
+        return "It's a palindrome"
+    else:
+        return "Sorry, it's not a palindrome."
+
+
+input = eval(input("Enter a number: "))
+
+print(isPalindrome(input))
