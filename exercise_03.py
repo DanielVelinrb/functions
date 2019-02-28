e03b28b254d0ba022153d74b1cd6fef52819a37f
Daniel <daniel.velin.2001@hotmail.com>
2019-02-28 10:28:23 -0500

    Complete exercise 3

---
+++ exercise_03.py
@@ -0,0 +1,17 @@
+def footToMeters(foot):
+    meter = foot * 0.305
+    return "{0:.3f}".format(meter)
+
+def metersToFoot(meter):
+    foot = meter / 0.305
+    return "{0:.3f}".format(foot)
+
+beggining = f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}"
+print(beggining)
+
+for i in range(1, 11):
+    j = 20 + 5 * (i - 1)
+
+    table = f"{i:<10}{footToMeters(i):<10}{'|':<10}{j:<10}{metersToFoot(j):<10}"
+
+    print(table)
