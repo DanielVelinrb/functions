354f8c245a8ab330dc10916cb064dbaccc0a1f01
Daniel <daniel.velin.2001@hotmail.com>
2019-03-01 22:18:01 -0500

    Fix function names in exercise 4

--- exercise_04.py
+++ exercise_04.py
@@ -1,19 +1,20 @@
-def foundPi(number):
+def found_pi(number):
     formula = 0
 
     for i in range(1, number + 1):
         formula += (-1) ** ( i + 1) / (2 * i - 1)
         pi = 4 * formula
-        
+
     return "{0:.4f}".format(pi)
 
 beggining = f"{'i':<25}m(i)\n"
 
 def main():
-    print(beggining)
-    for i in range(1, 902, 100):
-        to_print = f"{i:<25}{foundPi(i)}"
-        print(to_print)
+    if __name__ == "__main__":
+        print(beggining)
+        for i in range(1, 902, 100):
+            to_print = f"{i:<25}{found_pi(i)}"
+            print(to_print)
 
-if __name__ == "__main__":
-    main()
+
+main()
