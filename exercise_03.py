2dc5138e2efc644e4dae5183a8b090874d07de1d
Daniel <daniel.velin.2001@hotmail.com>
2019-03-01 22:17:47 -0500

    Fix function names in exercise 3

--- exercise_03.py
+++ exercise_03.py
@@ -1,22 +1,20 @@
-def footToMeters(foot):
+def foot_to_meters(foot):
     meter = foot * 0.305
     return "{0:.3f}".format(meter)
 
-def metersToFoot(meter):
+def meters_to_foot(meter):
     foot = meter / 0.305
     return "{0:.3f}".format(foot)
 
-beggining = f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}"
-print(beggining)
+def main():  
+    print(f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}")
 
-def main():
-    print(beggining)
-    for i in range(1, 11):
-        j = 20 + 5 * (i - 1)
+    if __name__ == "__main__":
+        for i in range(1, 11):
+                j = 20 + 5 * (i - 1)
+                table = f"{i:<10}{foot_to_meters(i):<10}{'|':<10}{j:<10}{meters_to_foot(j):<10}"
+                print(table)
+        
 
-        table = f"{i:<10}{footToMeters(i):<10}{'|':<10}{j:<10}{metersToFoot(j):<10}"
 
-        print(table)
-
-if __name__ == "__main__":
-    main()
+main()
