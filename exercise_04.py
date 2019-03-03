9b0ddffb0c9db8c91c97c0b8faebd96ab8f7b555
Daniel <daniel.velin.2001@hotmail.com>
2019-03-03 09:22:29 -0500

    Fix zero identation in exercise 4

--- exercise_04.py
+++ exercise_04.py
@@ -7,14 +7,12 @@ def found_pi(number):
 
     return "{0:.4f}".format(pi)
 
-beggining = f"{'i':<25}m(i)\n"
 
 def main():
-    if __name__ == "__main__":
-        print(beggining)
-        for i in range(1, 902, 100):
-            to_print = f"{i:<25}{found_pi(i)}"
-            print(to_print)
+    print(f"{'i':<25}m(i)\n")
+    for i in range(1, 902, 100):
+        to_print = f"{i:<25}{found_pi(i)}"
+        print(to_print)
 
-
-main()
+if __name__ == "__main__":
+    main()
