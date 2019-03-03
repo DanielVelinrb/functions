46a03dbe4ca962476a7b4a922da232d964e85ea6
Daniel <daniel.velin.2001@hotmail.com>
2019-03-03 09:22:15 -0500

    Fix zero identation in exercise 3

--- exercise_03.py
+++ exercise_03.py
@@ -8,13 +8,11 @@ def meters_to_foot(meter):
 
 def main():  
     print(f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}")
-
-    if __name__ == "__main__":
-        for i in range(1, 11):
-                j = 20 + 5 * (i - 1)
-                table = f"{i:<10}{foot_to_meters(i):<10}{'|':<10}{j:<10}{meters_to_foot(j):<10}"
-                print(table)
+    for i in range(1, 11):
+        j = 20 + 5 * (i - 1)
+        table = f"{i:<10}{foot_to_meters(i):<10}{'|':<10}{j:<10}{meters_to_foot(j):<10}"
+        print(table)
         
 
-
-main()
+if __name__ == "__main__":
+    main()
