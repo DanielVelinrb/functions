c947012ddfd4a35220aeaa4fa65d7a66c9d02709
Daniel <daniel.velin.2001@hotmail.com>
2019-03-04 11:03:14 -0500

    Fix blank lines' problem between functions in exercise 3

--- exercise_03.py
+++ exercise_03.py
@@ -2,17 +2,19 @@ def foot_to_meters(foot):
     meter = foot * 0.305
     return "{0:.3f}".format(meter)
 
+
 def meters_to_foot(meter):
     foot = meter / 0.305
     return "{0:.3f}".format(foot)
+
 
 def main():  
     print(f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}")
     for i in range(1, 11):
         j = 20 + 5 * (i - 1)
         table = f"{i:<10}{foot_to_meters(i):<10}{'|':<10}{j:<10}{meters_to_foot(j):<10}"
-        print(table)
-        
+        print(table)        
+
 
 if __name__ == "__main__":
     main()
