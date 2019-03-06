9898cee628bcdba03cb395485f26f41fe3761bb4
Daniel <daniel.velin.2001@hotmail.com>
2019-03-05 22:04:16 -0500

    Create function 'print_table' in exercise 3

--- exercise_03.py
+++ exercise_03.py
@@ -6,13 +6,18 @@ def meters_to_foot(meter):
     return f"{meter / 0.305:.3f}"
 
 
-def main():  
+def print_table(n):
     print(f"{'Feet':<10}{'Meter':<10}{'|':<10}{'Meter':<10}{'Feet':<10}")
-    for i in range(1, 11):
+    for i in range(1, n + 1):
         j = 20 + 5 * (i - 1)
         table = f"{i:<10}{foot_to_meters(i):<10}{'|':<10}{j:<10}{meters_to_foot(j):<10}"
-        print(table)        
+        print(table)
+    return table
 
+
+def main():  
+    print_table(10)
+            
 
 if __name__ == "__main__":
     main()
