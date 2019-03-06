2e2eb2e0b48f18668db839d5fe385898ae1b0da7
Daniel <daniel.velin.2001@hotmail.com>
2019-03-05 22:34:45 -0500

    Add function for display the table of pi in exercise 4

--- exercise_04.py
+++ exercise_04.py
@@ -5,15 +5,19 @@ def found_pi(number):
         formula += (-1) ** ( i + 1) / (2 * i - 1)
         pi = 4 * formula
 
-    return "{0:.4f}".format(pi)
+    return f"{pi:.4f}"
 
 
-def main():
+def pi_values_table(n):
     print(f"{'i':<25}m(i)\n")
-    for i in range(1, 902, 100):
+    for i in range(1, n + 1, 100):
         to_print = f"{i:<25}{found_pi(i)}"
         print(to_print)
 
+
+def main():
+    return pi_values_table(901)
+    
 
 if __name__ == "__main__":
     main()
