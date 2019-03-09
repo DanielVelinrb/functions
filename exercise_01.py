0a95298993673b4db2dadb174361a225696d3458
M <m@mathsistor.com>
2019-03-09 10:05:47 -0500

    Fix exercise 1

--- exercise_01.py
+++ exercise_01.py
@@ -1,22 +1,13 @@
-def pyramid_of_numbers(n):
-    n_to_print = ""
-    total = ""
+def print_pyramid_of_numbers(n):
+    temp = ''
     for i in range(1, n + 1):
-        #d_bet_numbers is the spaces that exist between each number
-        d_bet_numbers = int(len(str(i))) + 1
-        n_to_print = f"{i:{d_bet_numbers}}{n_to_print}"
-        space = "  " * (n - i)
-        if total == "":
-            total +=  f"{space}{n_to_print}"
-        else:
-            total +=  f"\n{space}{n_to_print}"
-
-    return total
+        temp = f'{i:<4}' + temp
+        print(('    ' * (n - i)) + temp)
 
 
 def main():
     number = eval(input("Enter a number: "))
-    print(pyramid_of_numbers(number))
+    print_pyramid_of_numbers(number)
 
 
 if __name__ == "__main__":
