b84f8f22ba977fbbbb74b8e8c43165af779adb8a
Daniel <daniel.velin.2001@hotmail.com>
2019-03-08 22:16:32 -0500

    Fix space between numbers for n > 10 in exercise 1

--- exercise_01.py
+++ exercise_01.py
@@ -2,12 +2,14 @@ def pyramid_of_numbers(n):
     n_to_print = ""
     total = ""
     for i in range(1, n + 1):
-        n_to_print = f"{i} {n_to_print}"
+        #d_bet_numbers is the spaces that exist between each number
+        d_bet_numbers = int(len(str(i))) + 1
+        n_to_print = f"{i:{d_bet_numbers}}{n_to_print}"
         space = "  " * (n - i)
         if total == "":
-            total =  f"{total}{space}{n_to_print}"
+            total +=  f"{space}{n_to_print}"
         else:
-            total =  f"{total}\n{space}{n_to_print}"
+            total +=  f"\n{space}{n_to_print}"
 
     return total
 
