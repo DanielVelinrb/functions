81a56fff5e9899acf562b9b21eb98e7b13a31677
M <m@mathsistor.com>
2019-03-09 19:44:52 -0500

    Fix naming issues and general spacing

--- exercise_01.py
+++ exercise_01.py
@@ -1,16 +1,15 @@
 def print_pyramid_of_numbers(n):
-    temp = ""
+    temp = ''
     for i in range(1, n + 1):
-        #sbn refers to the space between 
-        sbn = int(len(str(n))) + 1
-        temp = f'{i:<{sbn}}' + temp
-        print(("   " * (n - i)) + temp)
+        spacing = int(len(str(n))) + 1
+        temp = f'{i:<{spacing}}' + temp
+        print(' ' * spacing * (n - i) + temp)
 
 
 def main():
-    number = eval(input("Enter a number: "))
+    number = eval(input('Enter a number: '))
     print_pyramid_of_numbers(number)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
